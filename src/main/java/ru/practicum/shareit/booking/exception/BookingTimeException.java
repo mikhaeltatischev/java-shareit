7b178e27f4f1adbb3c070b79b8ba224383bd9b50
mikhaeltatischev <tatischevm@gmail.com>
2023-06-28 13:52:53 +0300

    fix: checkstyle

--- src/main/java/ru/practicum/shareit/booking/exception/BookingTimeException.java
+++ src/main/java/ru/practicum/shareit/booking/exception/BookingTimeException.java
@@ -5,7 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import java.time.LocalDateTime;
 
 @Slf4j
-public class BookingTimeException extends RuntimeException{
+public class BookingTimeException extends RuntimeException {
 
     public BookingTimeException(LocalDateTime start, LocalDateTime end) {
         super(getMessage(start, end));
